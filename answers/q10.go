651731102a868f693106565b7ee0e3c06e9a5ebc
Sam Gillespie <sammyseal90@gmail.com>
2022-12-10 16:07:13 +1000

    Solve Day 10

---
+++ answers/q10.go
@@ -0,0 +1,93 @@
+package answers
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func Day10() []interface{} {
+	data := ReadInputAsStr(10)
+	return []interface{}{q10part1(data), q10part2(data)}
+
+}
+
+func q10part1(data []string) int {
+	X := 1
+	cycle := 0
+	toAdd := 0
+	cursor := 0
+	signalStrength := 0
+	for {
+		if cycle == 20-1 || cycle == 60-1 || cycle == 100-1 || cycle == 140-1 || cycle == 180-1 || cycle == 220-1 {
+			signalStrength += (cycle + 1) * X
+		}
+		cycle++
+		if toAdd != 0 {
+			X += toAdd
+			toAdd = 0
+			continue
+		}
+		if cursor >= len(data) {
+			break
+		}
+		instruction := data[cursor]
+		cursor++
+		if instruction == "noop" {
+			continue
+		}
+		split := strings.Split(instruction, " ")
+		toAdd, _ = strconv.Atoi(split[1])
+	}
+
+	return signalStrength
+}
+
+func PrintMessage(data [][]rune) {
+	for _, row := range data {
+		fmt.Println(string(row))
+	}
+}
+
+func q10part2(data []string) int {
+	image := [][]rune{}
+	for i := 0; i < 6; i++ {
+		image = append(image, make([]rune, 40))
+	}
+	PrintMessage(image)
+	X := 1
+	cycle := 0
+	toAdd := 0
+	cursor := 0
+	for {
+		colPosition := cycle / 40
+		rowPosition := cycle % 40
+		if colPosition == 6 {
+			break
+		}
+		fmt.Println(rowPosition, colPosition, cycle, toAdd, cursor, X)
+		cycle++
+		if rowPosition >= X-1 && rowPosition <= X+1 {
+			image[colPosition][rowPosition] = '#'
+		} else {
+			image[colPosition][rowPosition] = '.'
+		}
+		if toAdd != 0 {
+			X += toAdd
+			toAdd = 0
+			continue
+		}
+		if cursor >= len(data) {
+			break
+		}
+		instruction := data[cursor]
+		cursor++
+		if instruction == "noop" {
+			continue
+		}
+		split := strings.Split(instruction, " ")
+		toAdd, _ = strconv.Atoi(split[1])
+	}
+	PrintMessage(image)
+	return 0
+}
--- main.go
+++ main.go
@@ -7,53 +7,60 @@ import (
 	"time"
 )
 
-var question int
+var QUESTION int
 var runProfile bool
 
 var questionMap = map[int]func() []interface{}{
-	1: answers.Day1,
-	2: answers.Day2,
-	3: answers.Day3,
-	4: answers.Day4,
-	5: answers.Day5,
-	6: answers.Day6,
-	7: answers.Day7,
-	8: answers.Day8,
-	9: answers.Day9,
+	1:  answers.Day1,
+	2:  answers.Day2,
+	3:  answers.Day3,
+	4:  answers.Day4,
+	5:  answers.Day5,
+	6:  answers.Day6,
+	7:  answers.Day7,
+	8:  answers.Day8,
+	9:  answers.Day9,
+	10: answers.Day10,
 }
 
 func main() {
 	parseArgs()
 	if runProfile == false {
-		result := SolveQuestion()
-		fmt.Printf("Day %d Part 1 Answer : %v\n", question, result[0])
-		fmt.Printf("Day %d Part 2 Answer : %v\n", question, result[1])
+		result := SolveQuestion(true, QUESTION)
+		fmt.Printf("Day %d Part 1 Answer : %v\n", QUESTION, result[0])
+		fmt.Printf("Day %d Part 2 Answer : %v\n", QUESTION, result[1])
 	} else {
-		runs := make([]time.Duration, 0, 1000)
-		for i := 0; i < 1000; i++ {
-			start := time.Now()
-			SolveQuestion()
-			runs = append(runs, time.Since(start))
+
+		for question := 1; question <= len(questionMap); question++ {
+			if question != QUESTION && QUESTION != 0 {
+				continue
+			}
+			runs := make([]time.Duration, 0, 1000)
+			for i := 0; i < 1000; i++ {
+				start := time.Now()
+				SolveQuestion(true, question)
+				runs = append(runs, time.Since(start))
+			}
+			var min, max, total time.Duration
+			for i, runtime := range runs {
+				if i == 0 {
+					min = runtime
+				}
+				if runtime < min {
+					min = runtime
+				}
+				if runtime > max {
+					max = runtime
+				}
+				total += runtime
+			}
+			avg := time.Duration(total.Nanoseconds() / int64(len(runs)))
+			fmt.Println("Q", question, "min:", min, "max:", max, "avg:", avg, "total", total)
 		}
-		var min, max, total time.Duration
-		for i, runtime := range runs {
-			if i == 0 {
-				min = runtime
-			}
-			if runtime < min {
-				min = runtime
-			}
-			if runtime > max {
-				max = runtime
-			}
-			total += runtime
-		}
-		avg := time.Duration(total.Nanoseconds() / int64(len(runs)))
-		fmt.Println("min:", min, "max:", max, "avg:", avg, "total", total)
 	}
 }
 
-func SolveQuestion() []interface{} {
+func SolveQuestion(silent bool, question int) []interface{} {
 	if question == 0 {
 		times := []time.Duration{}
 		for i := 1; i <= len(questionMap); i++ {
@@ -61,13 +68,18 @@ func SolveQuestion() []interface{} {
 			questionMap[i]()
 			end := time.Since(start)
 			times = append(times, end)
-			fmt.Printf("Day %d: Time Taken %s\n", i, end)
+			if !silent {
+				fmt.Printf("Day %d: Time Taken %s\n", i, end)
+			}
+
 		}
 		var totalDuration time.Duration
 		for _, dur := range times {
 			totalDuration += dur
 		}
-		fmt.Printf("Total Time Taken: %s\n\n", totalDuration)
+		if !silent {
+			fmt.Printf("Total Time Taken: %s\n\n", totalDuration)
+		}
 		return []interface{}{0, 0}
 	} else {
 		return questionMap[question]()
@@ -75,7 +87,7 @@ func SolveQuestion() []interface{} {
 }
 
 func parseArgs() {
-	flag.IntVar(&question, "question", 0, "Which question to answer")
+	flag.IntVar(&QUESTION, "question", 0, "Which question to answer")
 	flag.BoolVar(&runProfile, "prof", false, "Whether to run a profile. If enabled runs the solution 1000 times and grabs an average, min and max runtimes")
 	flag.Parse()
 
